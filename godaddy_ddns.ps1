605ea6979c95178caf3cc779dc58b991cffdbdcd
sflamm <sflamm@ogeestudio.com>
2019-04-02 22:06:01 -0700

    Update godaddy_ddns.ps1
    Corrected... previous did not work

--- godaddy_ddns.ps1
+++ godaddy_ddns.ps1
@@ -13,18 +13,17 @@ $name = 'name_of_host' #name of the A record to update
 $key = 'key' #key for godaddy developer API
 $secret = 'Secret' #Secret for godday developer API
 
-$headers = @{}
-$headers["Authorization"] = 'sso-key ' + $key + ':' + $secret
-$result = Invoke-WebRequest https://api.godaddy.com/v1/domains/$domain/records/A/$name -method get -headers $headers
+$Headers = @{}
+$Headers["Authorization"] = 'sso-key ' + $apiKey + ':' + $apiSecret
+
+$result = Invoke-WebRequest https://api.godaddy.com/v1/domains/$domain/records/A/$name -Method Get -Headers $Headers
 $content = ConvertFrom-Json $result.content
 $dnsIp = $content.data
-
-# Get public ip address there are several websites that can do this.
-$currentIp = Invoke-RestMethod http://ipinfo.io/json | Select -exp ip
+$currentIp = Invoke-RestMethod http://ipinfo.io/json | Select -exp ip
 
 if ( $currentIp -ne $dnsIp) {
-    $Request = @{ttl=3600;data=$currentIp }
-    $JSON = Convertto-Json $request
-    Invoke-WebRequest https://api.godaddy.com/v1/domains/$domain/records/A/$name -method put -headers $headers -Body $json -ContentType "application/json"
+    $json = ConvertTo-Json @(@{data=$currentIp;ttl=600})
+    Invoke-WebRequest https://api.godaddy.com/v1/domains/$domain/records/A/$name -Method put -headers $headers -Body $json -ContentType 'application/json'
 }
-
+# Print Update
+# Invoke-WebRequest https://api.godaddy.com/v1/domains/$domain/records/A/$name -Method Get -Headers $Headers | ConvertFrom-Json
